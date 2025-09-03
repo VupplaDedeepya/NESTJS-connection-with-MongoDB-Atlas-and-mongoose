408962ae43fdd0f7cc15b5e5622f47cc08694b2f
VupplaDedeepya <vuppaladedeepya@gmail.com>
2025-09-03 11:12:37 +0530

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api'); // Optional: sets prefix like /api/blog
-  app.enableCors(); // Enable CORS if you want to connect from frontend
+  app.enableCors({
+     origin: ['http://localhost:3000'], // your Next.js frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }); // Enable CORS if you want to connect from frontend
 
-  await app.listen(3009);
+  await app.listen(3005);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
